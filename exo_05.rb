363851753279ee1812b4df83bbf1c341772d045c
dujulien <du.julien1@gmail.com>
2019-10-01 12:30:55 +0200

    Exos 4 to 6

---
+++ exo_05.rb
@@ -0,0 +1,26 @@
+puts "On va compter le nombre d'heures de travail à THP"
+puts "Travail : #{10 * 5 * 11}"
+# #{} convertit un nombre en chaine de caratère et permet ainsi
+# de concaténer une chaine de caractère et un nombre
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}"
+# Revoie "En minutes ça fait 33000"
+puts "Et en secondes ?"
+# Renvoie "Et en secondes"
+puts 10 * 5 * 11 * 60 * 60
+# Renvoie "1980000"
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+# Renvoie "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+puts 3 + 2 < 5 - 7
+# Effectue le test 5 < -2 et renvoie False
+puts "Ça fait combien 3 + 2 ? #{3 + 2}"
+# Renvoie "Ça fait combien 3 + 2 ? 5"
+puts "Ça fait combien 5 - 7 ? #{5 - 7}"
+# Renvoie "Ça fait combien 5 - 7 ? -2"
+puts "Ok, c'est faux alors !"
+# Renvoie "Ok, c'est faux alors !"
+puts "C'est drôle ça, faisons-en plus :"
+# Renvoie "C'est drôle ça, faisons-en plus :"
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"
+# Renvoie le résultat du booléen contenu entre les accolades et l'associe à la chaine de caractère qui précède
